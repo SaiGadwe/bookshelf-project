90e7ebebf1d7e68fb3056dccfa665763de9c3d15
SaiGadwe <saigadwe369@gmail.com>
2026-07-22 17:33:34 +0530

    add books app with Book model

---
+++ bookshelf_project/books/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+    added_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
