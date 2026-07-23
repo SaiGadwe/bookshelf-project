ce658114684368d36a9bd63f723b6a7b47fee61c
SaiGadwe <saigadwe369@gmail.com>
2026-07-23 17:30:13 +0530

    add views, urls and template for book list page

--- bookshelf_project/urls.py
+++ bookshelf_project/urls.py
@@ -2,8 +2,9 @@
 URL configuration for bookshelf_project project.
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('books.urls')),
 ]
