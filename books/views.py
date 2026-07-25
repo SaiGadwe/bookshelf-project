24149adbe227c9ad8f8730b929747d490846a211
SaiGadwe <saigadwe369@gmail.com>
2026-07-25 15:45:50 +0530

    add form to add new books

--- books/views.py
+++ books/views.py
@@ -2,12 +2,25 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from .models import Book
+from .forms import BookForm
 
 
 @login_required
 def book_list(request):
     books = Book.objects.all().order_by('-added_on')
     return render(request, 'books/book_list.html', {'books': books})
+
+
+@login_required
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'books/add_book.html', {'form': form})
 
 
 def logout_view(request):
